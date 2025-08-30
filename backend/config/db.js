be7cc0d68a7c2d73cdaf26bf8ed5d7f16a458802
DelwinPrakash <delwinprakash499@gmail.com>
2025-08-30 13:55:58 +0530

    postgresql config impl

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_NAME,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+});
+
+const connectDB = async () => {
+    try{
+        await pool.connect();
+        console.log("Connected to PostgreSQL database");
+    }catch(error){
+        console.error("Database connection error:", error);
+    }
+}
+
+module.exports = connectDB;
