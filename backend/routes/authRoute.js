26601f73469810f417cf62a5fe78da13b94f6872
DelwinPrakash <delwinprakash499@gmail.com>
2025-08-30 19:15:22 +0530

    auth routes impl

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { handleLogin, handleRegister } from "../controllers/authController";
+
+const router = express.Router();
+
+router.post("/login", handleLogin);
+router.post("/signup", handleRegister);
+
+export default router;
