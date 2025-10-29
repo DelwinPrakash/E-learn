73a5b484bf660f1c3d0cafc50aef135268f01ae0
DelwinPrakash <delwinprakash499@gmail.com>
2025-10-29 21:53:19 +0530

    Add JWT decoding and implement authGuard for route protection

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { EmailverificationComponent } from './emailverification/emailverification.component';
+import { authGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'verify-email', component: EmailverificationComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'flashcard', component: FlashcardComponent },
   { path: 'leaderboard', component: LeaderboardComponent },
   { path: '**', redirectTo: '/dashboard' } // Wildcard route
--- frontend/src/app/guard/auth.guard.ts
+++ frontend/src/app/guard/auth.guard.ts
@@ -1,11 +1,15 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   
   if(!authService.isLoggedIn()){
+    const router = inject(Router);
+
+    router.navigate(['/login']);
+    
     return false;
   }
   return true;
