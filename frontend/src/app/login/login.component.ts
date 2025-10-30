4f922381a6d834ffaa23917e4efd9536252dfe3f
DelwinPrakash <delwinprakash499@gmail.com>
2025-10-30 22:50:09 +0530

    Refactor login and signup components to use HttpClient for API calls and update alert messages

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../services/login.service'; 
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,21 +14,33 @@ export class LoginComponent {
   email = '';
   password = '';
 
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(private loginService: LoginService, private router: Router, private http: HttpClient, private authService: AuthService) {}
 
   onLogin() {
     const credentials = { email: this.email, password: this.password };
 
-    this.loginService.login(credentials).subscribe({
+    // this.loginService.login(credentials).subscribe({
+    //   next: (res) => {
+    //     localStorage.setItem('e_learning_token', res.token); 
+    //     localStorage.setItem('e_learning_session', res.newSession.session_id); 
+    //     alert('Login successful ✅');
+    //     this.router.navigate(['/dashboard']);
+    //   },
+    //   error: (err) => {
+    //     console.log(err);
+    //     alert(err.error.message || 'Login failed ❌');
+    //   }
+    // });
+
+    this.http.post<any>(`${environment.BACKEND_BASE_URL}/api/auth/login`, credentials).subscribe({
       next: (res) => {
-        localStorage.setItem('e_learning_token', res.token); 
-        localStorage.setItem('e_learning_session', res.newSession.session_id); 
-        alert('Login successful ✅');
+        this.authService.setToken(res.token);
+        alert("Login successful!");
         this.router.navigate(['/dashboard']);
       },
       error: (err) => {
         console.log(err);
-        alert(err.error.message || 'Login failed ❌');
+        alert(err.error.message || "Login failed!");
       }
     });
   }
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { SignupService } from '../services/signup.service'; 
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-signup',
@@ -11,16 +14,26 @@ export class SignupComponent {
   email = '';
   password = '';
 
-  constructor(private signupService: SignupService) {}
+  constructor(private signupService: SignupService, private http: HttpClient, private router: Router) {}
 
   onSignup() {
-    const userData = { name: this.name, email: this.email, password: this.password };
+    const credentials = { name: this.name, email: this.email, password: this.password };
 
-    this.signupService.signup(userData).subscribe({
+    // this.signupService.signup(userData).subscribe({
+    //   next: (res) => {
+    //     alert('Signup successful, verify your email 🎉');
+    //   },
+    //   error: (err) => {
+    //     alert(err.error.message || 'Signup failed ❌');
+    //   }
+    // });
+
+    this.http.post<any>(`${environment.BACKEND_BASE_URL}/api/auth/signup`, credentials).subscribe({
       next: (res) => {
         alert('Signup successful, verify your email 🎉');
       },
       error: (err) => {
+        console.log(err);
         alert(err.error.message || 'Signup failed ❌');
       }
     });
