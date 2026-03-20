841f43bf5e38f567124016c5b2652d72595ffecb
DelwinPrakash <delwinprakash499@gmail.com>
2026-03-20 15:56:21 +0530

    feat: Add an API endpoint and controller function to allow teachers and admins to create quizzes.

--- backend/routes/quizRoute.js
+++ backend/routes/quizRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getTopics, getQuestions, singlePlayerResult } from "../controllers/quizController.js";
+import { getTopics, getQuestions, singlePlayerResult, createQuiz } from "../controllers/quizController.js";
 import { verifyJWT } from "../middlewares/verifyJWT.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/topics", getTopics);
 router.get("/questions", verifyJWT, getQuestions);
 router.post("/single-player-result", verifyJWT, singlePlayerResult);
+router.post("/create", verifyJWT, createQuiz);
 
 export default router;
